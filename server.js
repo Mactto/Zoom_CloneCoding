cfcd060dc34d3f9ce11012dfb6ade89d2aff040e
Mactto <shc9928@gmail.com>
2021-04-18 20:12:07 +0900

    개발환경 세팅

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const { v4: uuidv4 } = require('uuid');
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.redirect(`/${uuidv4()}`)
+})
+
+app.get('/:room', (req, res) => {
+    res.render('room', { roomId: req.params.room })
+})
+
+app.listen(5000, () => {
+    console.log("Server is Working on 5000 port");
+})
